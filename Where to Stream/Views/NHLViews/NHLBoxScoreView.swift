688600acc2966e5a375301f2d9d13d9d517147aa
Bill Faucett <billfaucett@gmail.com>
2024-02-01 09:39:50 -0500

    Initial Line Score and SoG in Box Score

--- Where to Stream/Views/NHLViews/NHLBoxScoreView.swift
+++ Where to Stream/Views/NHLViews/NHLBoxScoreView.swift
@@ -13,6 +13,12 @@ struct NHLBoxScoreView: View {
     @ObservedObject var nhlViewControler = NHLViewController()
     @State var boxScoreData: GameDetails?
     
+    func refereeList(_ referees: [Venue]) -> String {
+        return referees
+            .map { "\($0.venueDefault)" }
+            .joined(separator: ", ")
+    }
+    
     var body: some View {
         VStack(alignment: .leading){
             HStack{
@@ -25,23 +31,89 @@ struct NHLBoxScoreView: View {
                 Text(String(boxScoreData?.homeTeam.score ?? 0))
                 Text("SOG: \(String(boxScoreData?.homeTeam.sog ?? 0))")
             }
-            VStack{
-                Text(boxScoreData?.boxscore.gameReports.eventSummary ?? "")
-                let highlightsUrl = URL(string: "https://www.nhl.com\(urlString)")
-                Button(action: {
-                    UIApplication.shared.open(highlightsUrl!)
-                }) {
-                    Text("Highlights")
-                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                        .font(.caption)
+            
+            VStack {
+                Text("Score by Period")
+                HStack {
+                    Text(boxScoreData?.awayTeam.abbrev ?? "")
+                        .frame(width: 50, alignment: .leading)
+                    if let byPeriod = boxScoreData?.boxscore.linescore.byPeriod {
+                        ForEach(byPeriod.indices, id: \.self) { index in
+                            let period = byPeriod[index]
+                            Text(String(period.away))
+                                .frame(width: 20, alignment: .leading)
+                        }
+                        Text(String(boxScoreData?.boxscore.linescore.totals.away ?? 0))
+                            .frame(width: 20, alignment: .leading)
+                    }
+                }
+                
+                VStack {
+                    HStack {
+                        Text("SOG")
+                            .frame(width: 50, alignment: .leading)
+                        if let shotsByPeriod = boxScoreData?.boxscore.shotsByPeriod {
+                            ForEach(shotsByPeriod.indices, id: \.self) { index in
+                                let shots = shotsByPeriod[index]
+                                Text(String(shots.away))
+                                    .frame(width: 20, alignment: .leading)
+                            }
+                        }
+                        Text(String(boxScoreData?.awayTeam.sog ?? 0))
+                    }
+                }
+                
+                HStack {
+                    Text(boxScoreData?.homeTeam.abbrev ?? "")
+                        .frame(width: 50, alignment: .leading)
+                    if let byPeriod = boxScoreData?.boxscore.linescore.byPeriod {
+                        ForEach(byPeriod.indices, id: \.self) { index in
+                            let period = byPeriod[index]
+                            Text(String(period.home))
+                                .frame(width: 20, alignment: .leading)
+                        }
+                        Text(String(boxScoreData?.boxscore.linescore.totals.home ?? 0))
+                            .frame(width: 20, alignment: .leading)
+                        
+                    }
+                }
+                
+                VStack {
+                    HStack {
+                        Text("SOG")
+                            .frame(width: 50, alignment: .leading)
+                        if let shotsByPeriod = boxScoreData?.boxscore.shotsByPeriod {
+                            ForEach(shotsByPeriod.indices, id: \.self) { index in
+                                let shots = shotsByPeriod[index]
+                                Text(String(shots.home))
+                                    .frame(width: 20, alignment: .leading)
+                            }
+                        }
+                        Text(String(boxScoreData?.homeTeam.sog ?? 0))
+                    }
+                }
+                
+                VStack{
+                    Text("Referees: \(refereeList(boxScoreData?.boxscore.gameInfo.referees ?? []))")
+                        .font(.caption2)
+                    Text("Linesmen: \(refereeList(boxScoreData?.boxscore.gameInfo.linesmen ?? []))")
+                        .font(.caption2)
+                    let highlightsUrl = URL(string: "https://www.nhl.com\(urlString)")
+                    Button(action: {
+                        UIApplication.shared.open(highlightsUrl!)
+                    }) {
+                        Text("Highlights")
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            .font(.subheadline)
+                    }
                 }
             }
-        }
-        .onAppear {
-            self.nhlViewControler.getBoxScore(gameId: gameId)
-        }
-        .onReceive(nhlViewControler.$boxScore){ newBoxScore in
-            self.boxScoreData = newBoxScore
+            .onAppear {
+                self.nhlViewControler.getBoxScore(gameId: gameId)
+            }
+            .onReceive(nhlViewControler.$boxScore){ newBoxScore in
+                self.boxScoreData = newBoxScore
+            }
         }
     }
 }
