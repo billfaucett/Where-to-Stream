c8126b08037f4013e663910e47b2cc2caa8c9c2b
Bill Faucett <billfaucett@gmail.com>
2024-02-07 10:49:20 -0500

    Uses modular design for SA search and details

--- Where to Stream/Views/StreamingViews/Updated Architecture/SAResultListView.swift
+++ Where to Stream/Views/StreamingViews/Updated Architecture/SAResultListView.swift
@@ -14,38 +14,11 @@ struct SAResultListView: View {
     @State var plot: String = ""
     
     var body: some View {
-        Section (header: Text("Results")) {
+        Section (header: Text("Search Results").bold().font(.headline)) {
             if let resultsList = resultsList?.result {
                 List(resultsList.indices, id: \.self) { index in
                     if resultsList[index].title.contains(searchText!) && ((resultsList[index].streamingInfo?.us?.first) != nil) {
-                        VStack {
-                            HStack{
-                                Text(resultsList[index].title)
-                                    .bold()
-                                    .font(.title2)
-                                Spacer()
-                                if resultsList[index].imdbId == omdbResult?.imdbID {
-                                    SAImageView(urlString: omdbResult!.Poster)
-                                }
-                            }
-                        }
-                        HStack {
-                            VStack {
-                                Text(resultsList[index].type.capitalized)
-                                Text(resultsList[index].genres.first?.name ?? "Genre")
-                            }
-                            Spacer()
-                            VStack {
-                                Text(resultsList[index].streamingInfo?.us?.first?.capitalizedServiceName ?? "Service Name")
-                                Text(resultsList[index].streamingInfo?.us?.first?.capitalizedStreamingType ?? "Service Type")
-                            }
-                            
-                        }
-                        VStack {
-                            if resultsList[index].imdbId == omdbResult?.imdbID {
-                                Text(omdbResult?.Plot.description ?? "")
-                            }
-                        }
+                        SATitleDetailsView(programDetails: resultsList[index], omdbDetails: omdbResult)
                     }
                 }
             }
--- Where to Stream/Views/StreamingViews/Updated Architecture/SATitleDetailsView.swift
+++ Where to Stream/Views/StreamingViews/Updated Architecture/SATitleDetailsView.swift
@@ -21,23 +21,28 @@ struct SATitleDetailsView: View {
                             .font(.title2)
                             .bold()
                         Spacer()
-                        if omdbDetails != nil {
+                        if (omdbDetails != nil && omdbDetails?.imdbID == programDetails?.imdbId){
                             SAImageView(urlString: omdbDetails!.Poster)
                         }
                     }
                     VStack{
+                        Divider()
                         HStack {
                             VStack{
                                 Text(programDetails?.type.capitalized ?? "Service")
                                 Text(programDetails?.genres.first?.name ?? "genre")
                             }
+                            Spacer()
                             VStack {
                                 Text(programDetails?.streamingInfo?.us?.first?.capitalizedServiceName ?? "service")
                                 Text(programDetails?.streamingInfo?.us?.first?.capitalizedStreamingType ?? "type")
                             }
                         }
-                        if omdbDetails != nil {
-                            Text(omdbDetails!.Plot)
+                        VStack {
+                            if (omdbDetails != nil && omdbDetails?.imdbID == programDetails?.imdbId) {
+                                Divider()
+                                Text(omdbDetails!.Plot)
+                            }
                         }
                     }
                 }
