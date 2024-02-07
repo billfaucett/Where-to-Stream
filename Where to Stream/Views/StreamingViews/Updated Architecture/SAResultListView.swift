b2f605846008c68c9d46428a7db0487fdfef3941
Bill Faucett <billfaucett@gmail.com>
2024-02-06 19:28:10 -0500

    Bold Title

--- Where to Stream/Views/StreamingViews/Updated Architecture/SAResultListView.swift
+++ Where to Stream/Views/StreamingViews/Updated Architecture/SAResultListView.swift
@@ -19,11 +19,13 @@ struct SAResultListView: View {
                     if resultsList[index].title.contains(searchText!) && ((resultsList[index].streamingInfo?.us?.first) != nil) {
                         HStack {
                             Text("\(resultsList[index].title) - \(resultsList[index].type)")
+                                .bold()
                             Spacer()
                             VStack {
                                 Text(resultsList[index].streamingInfo?.us?.first?.capitalizedServiceName ?? "Service Name")
                                 Text(resultsList[index].streamingInfo?.us?.first?.capitalizedStreamingType ?? "Service Type")
                             }
+                            
                         }
                         VStack {
                             if resultsList[index].imdbId == omdbResult?.imdbID {
