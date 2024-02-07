07b0caa8cefb3cbaeb0c94de43c4b936efb9d232
Bill Faucett <billfaucett@gmail.com>
2024-02-07 10:39:30 -0500

    SA Deatil View Initial Commit

--- Where to Stream/Views/StreamingViews/Updated Architecture/SATitleDetailsView.swift
+++ Where to Stream/Views/StreamingViews/Updated Architecture/SATitleDetailsView.swift
@@ -10,14 +10,37 @@ import SwiftUI
 struct SATitleDetailsView: View {
     //@ObservedObject var saViewController : SASearchViewController
     var programDetails: ProgramDetails?
-    @State private var image: UIImage? = nil
+    var omdbDetails: OMdbModelResult?
     
     var body: some View {
         NavigationView {
             Section(header: Text("Program Details")) {
-                Text(programDetails?.title ?? "title")
-                Text(programDetails?.genres.first?.name ?? "genre")
-                Text("\(programDetails?.streamingInfo?.us?.first?.capitalizedServiceName ?? "service") - \(programDetails?.streamingInfo?.us?.first?.capitalizedStreamingType ?? "type")")
+                VStack{
+                    HStack{
+                        Text(programDetails?.title ?? "title")
+                            .font(.title2)
+                            .bold()
+                        Spacer()
+                        if omdbDetails != nil {
+                            SAImageView(urlString: omdbDetails!.Poster)
+                        }
+                    }
+                    VStack{
+                        HStack {
+                            VStack{
+                                Text(programDetails?.type.capitalized ?? "Service")
+                                Text(programDetails?.genres.first?.name ?? "genre")
+                            }
+                            VStack {
+                                Text(programDetails?.streamingInfo?.us?.first?.capitalizedServiceName ?? "service")
+                                Text(programDetails?.streamingInfo?.us?.first?.capitalizedStreamingType ?? "type")
+                            }
+                        }
+                        if omdbDetails != nil {
+                            Text(omdbDetails!.Plot)
+                        }
+                    }
+                }
             }
         }
     }
