5cd203fa67db77b3f120c8519e3934224aa172c8
Bill Faucett <billfaucett@gmail.com>
2024-02-12 14:16:54 -0500

    Preview Providers for SA List and Details Views

--- Where to Stream/Views/StreamingViews/SearchAvailibilityViews/SAResultListView.swift
+++ Where to Stream/Views/StreamingViews/SearchAvailibilityViews/SAResultListView.swift
@@ -14,7 +14,7 @@ struct SAResultListView: View {
     var searchText: String?
     
     var body: some View {
-        Section (header: Text("Search Results").bold().font(.headline)) {
+        Section (header: Text("Search Results").bold().font(.title)) {
             if let resultsList = resultsList?.result {
                 List(resultsList.indices, id: \.self) { index in
                     if resultsList[index].title.contains(searchText!) && ((resultsList[index].streamingInfo?.us?.first) != nil) {
@@ -25,9 +25,16 @@ struct SAResultListView: View {
         }
     }
 }
-
-#Preview {
-    /*static let myResults = ProgramResults(result: ProgramDetails(firstAirYear: 2021, originalTitle: "Jenny and Billy's Adventures", creators: nil, imdbId: "tt2802850", tmdbId: 123, type: "RomCom", title: "Jenny and Billy's Adventures", lastAirYear: 2024, genres: [Genre](), seasonCount: 4, episodeCount: 100, status: Status(statusText: "Active", statusCode: 69), streamingInfo: StreamingInfo(us: StreamingOption(audios: nil, subtitles: nil, streamingType: "Subscription", availableSince: 2021, service: "Netflix", link: nil))))*/
+ 
+struct SAResultListView_Previews: PreviewProvider {
+    static var title = ProgramDetails(firstAirYear: 2021, originalTitle: "Jenny and Billy's Adventures", creators: ["Bill","Jen"], imdbId: "tt2802850", tmdbId: 123, type: "Show", title: "Jenny and Billy's Adventures", lastAirYear: 2024, genres: [Genre(id: 1, name: "RomCom")], seasonCount: 4, episodeCount: 100, status: Status(statusText: "status", statusCode: 2), streamingInfo: StreamingInfo(us: [StreamingOption(audios: nil, subtitles: nil, streamingType: "Subscription", availableSince: 2021, service: "Netflix", link: nil)]), omdbResult: omdb)
     
-    SAResultListView()
+    static var results = ProgramResults.init(result: [title])
+    
+    static var omdb = OMdbModelResult(Title: "Jenny and Billy's Adventures", Year: "2021", Rated: "R", Released: "2021", Runtime: "60", Genre: "RomCom", Director: "BF", Writer: "JB", Actors: "JB, BF", Plot: "You get it", Language: "en", Country: "us", Awards: "Engagement", Poster: "https://m.media-amazon.com/images/M/MV5BZjZjMzQ2ZmUtZWEyZC00NWJiLWFjM2UtMzhmYzZmZDcxMzllXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg", Ratings: [OMdbModelResult.Rating(Source: "Rating Source", Value: "100")], Metascore: "10", imdbRating: "10", imdbVotes: "100", imdbID: "tt2802850", totalSeasons: "10", Response: "Response")
+    
+    static var previews: some View {
+        SAResultListView(resultsList: results, searchText: "Jenny and Billy's Adventures")
+    }
 }
+
--- Where to Stream/Views/StreamingViews/SearchAvailibilityViews/SATitleDetailsView.swift
+++ Where to Stream/Views/StreamingViews/SearchAvailibilityViews/SATitleDetailsView.swift
@@ -14,7 +14,6 @@ struct SATitleDetailsView: View {
     
     var body: some View {
         if let program = programDetails, let omdbInfo = programDetails?.omdbResult {
-            //Text("Program Title: \(program.title) Omdb Title: \(omdbInfo.imdbID)")
             NavigationView {
                 Section(header: Text("Program Details: \(program.title)")) {
                     VStack{
@@ -55,15 +54,21 @@ struct SATitleDetailsView: View {
                     }
                 }
             }
+            .navigationTitle("Program Info:")
         }
     }
 }
 
 struct SATitleDetailsView_Previews: PreviewProvider {
-    static let programDetails = ProgramDetails(firstAirYear: 2024, originalTitle: "Billy Loves Jenny", creators: ["BF","JB"], imdbId: "tt2802850", tmdbId: 222, type: "RomCom", title: "Billy and Jenny Get Down", lastAirYear: 2024, genres: [Genre](), seasonCount: 1, episodeCount: 1, status: Status(statusText: "Done", statusCode: 1), streamingInfo: nil)
+    static let programDetails = ProgramDetails(firstAirYear: 2024, originalTitle: "Billy Loves Jenny", creators: ["BF","JB"], imdbId: "tt2802850", tmdbId: 222, type: "RomCom", title: "Billy and Jenny Get Down", lastAirYear: 2024, genres: [Genre](), seasonCount: 1, episodeCount: 1, status: Status(statusText: "Done", statusCode: 1), streamingInfo: StreamingInfo(us: [StreamingOption(audios: nil, subtitles: nil, streamingType: "Subscription", availableSince: 2021, service: "Netflix", link: nil)]), omdbResult: omdbDetails)
+    
     static let omdbDetails = OMdbModelResult(Title: "Billy and Jenn Get Down", Year: "2024", Rated: "XXX", Released: "2024", Runtime: "120", Genre: "Porn", Director: "BF", Writer: "JB", Actors: "Bf, JB", Plot: "Jenny and Billy meet and get busy", Language: "en", Country: "us", Awards: "", Poster: "https://m.media-amazon.com/images/M/MV5BZjZjMzQ2ZmUtZWEyZC00NWJiLWFjM2UtMzhmYzZmZDcxMzllXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg", Ratings: [OMdbModelResult.Rating](), Metascore: "10", imdbRating: "10", imdbVotes: "100", imdbID: "tt2802850", totalSeasons: nil, Response: "success")
     
     static var previews: some View {
-        SATitleDetailsView()
+        SATitleDetailsView(programDetails: programDetails, omdbDetails: omdbDetails)
+            .previewDisplayName("With Details")
+        
+        SATitleDetailsView(programDetails: nil, omdbDetails: nil)
+            .previewDisplayName("No Details")
     }
 }
