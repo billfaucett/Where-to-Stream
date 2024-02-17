c1a5a3ab6417193b8c219eb8961e8b591917a104
Bill Faucett <billfaucett@gmail.com>
2024-02-17 14:41:54 -0500

    fornatting

--- Where to Stream/Views/NHLViews/NHLGoalieStatListView.swift
+++ Where to Stream/Views/NHLViews/NHLGoalieStatListView.swift
@@ -13,8 +13,8 @@ struct NHLGoalieStatListView: View {
     var body: some View {
         VStack {
             let maxGamesPlayed = stats.data.map { $0.gamesPlayed }.max() ?? 0
-            let sortedByGAA = stats.data.sorted{$0.goalsAgainstAverage < $1.goalsAgainstAverage}
-                    .filter{ $0.gamesPlayed >= 5 }
+            let sortedByGAA = stats.data.sorted { $0.goalsAgainstAverage < $1.goalsAgainstAverage }
+                    .filter { $0.gamesPlayed >= 5 }
                     .prefix(25)
 
             Section(header: Text("NHL Top 25 Goalies")){
